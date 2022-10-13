0900c36046e0e0e46ed33d751c2508c7697e872d
Nahomy Castro <98111102+lemoonchild@users.noreply.github.com>
2022-10-13 17:54:22 -0600

    Update trabajadores.java

--- trabajadores.java
+++ trabajadores.java
@@ -8,7 +8,15 @@ public class trabajadores {
     private int salarioBase;
     private int ausencias_impuntualidades;
     private int tarifa_diaria;
-
+    
+    //Constructores de la clase padre 
+    public trabajadores() {
+        this.nombre = "";
+        this.identificación = "";
+        this.salarioBase = 0;
+        this.ausencias_impuntualidades = 0;
+        this.tarifa_diaria = salarioBase / 24;
+    }
     public trabajadores(String nombre, String identificación, int salarioBase, int ausencias_impuntualidades) {
         this.nombre = nombre;
         this.identificación = identificación;
@@ -16,45 +24,54 @@ public class trabajadores {
         this.ausencias_impuntualidades = ausencias_impuntualidades;
         this.tarifa_diaria = salarioBase / 24;
     }
-
+    //Getters y Setters de los atributos del trabajador
     public String getNombre() {
         return nombre;
     }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
     public String getIdentificación() {
         return identificación;
     }
-
     public void setIdentificación(String identificación) {
         this.identificación = identificación;
     }
-
     public int getSalarioBase() {
         return salarioBase;
     }
-
     public void setSalarioBase(int salarioBase) {
         this.salarioBase = salarioBase;
     }
-
     public int getAusencias_impuntualidades() {
         return ausencias_impuntualidades;
     }
-
     public void setAusencias_impuntualidades(int ausencias_impuntualidades) {
         this.ausencias_impuntualidades = ausencias_impuntualidades;
     }
-
     public int getTarifa_diaria() {
         return tarifa_diaria;
     }
-
     public void setTarifa_diaria(int tarifa_diaria) {
         this.tarifa_diaria = tarifa_diaria;
     }
+    
+     //Calculos del salario promedio de los trabadores que será heredado por las clases hijas 
+    /**
+     * @return la cantidad de salario que gana un trabajador promedio
+     */
+    public double salario(){ 
+
+		return salarioBase;
+
+	}
+    /**
+     * @return el descuento por las ausencias multiplicado con la tarifa diaria del trabajador 
+     */
+    public double descuentoAusencias(){
+
+		return tarifa_diaria*ausencias_impuntualidades;
+
+	}
 
 }
