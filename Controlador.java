25e9282054dafc9b163ac28bdf6f7fbb2134ad76
Maria-Villafuerte <100331510+Maria-Villafuerte@users.noreply.github.com>
2022-10-12 20:38:27 -0600

    Controlador para las estadísticas de salario

---
+++ Controlador.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Controlador {
+    boolean Es_Docente;
+    Trabajador trabajador; 
+    ArrayList<Trabajador> Trabajador_ingresados = new ArrayList<>();
+
+
+    public void Verificar_tipo_de_Trabajador (){
+    }
+    public void Crear_trabajador(boolean Es_Docente){
+    }
+    public void Salario(boolean Es_Docente){
+    }
+    public void Imprimir_Trabajadores_en_lista (){
+    }
+}
