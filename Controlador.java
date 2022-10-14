7e287c23812078670ec7abac9732d3a9409718a8
Fabiola Contreras <98127191+Fabiola-cc@users.noreply.github.com>
2022-10-14 11:16:53 -0600

    cambios por funcionamiento

--- Controlador.java
+++ Controlador.java
@@ -2,16 +2,18 @@ import java.util.ArrayList;
 
 public class Controlador {
     boolean Es_Docente;
-    Trabajador trabajador; 
-    ArrayList<Trabajador> Trabajador_ingresados = new ArrayList<>();
+    trabajadores trabajador;
+    ArrayList<trabajadores> Trabajador_ingresados = new ArrayList<>();
 
+    public void Verificar_tipo_de_Trabajador() {
+    }
 
-    public void Verificar_tipo_de_Trabajador (){
+    public void Crear_trabajador(boolean Es_Docente) {
     }
-    public void Crear_trabajador(boolean Es_Docente){
+
+    public void Salario(boolean Es_Docente) {
     }
-    public void Salario(boolean Es_Docente){
-    }
-    public void Imprimir_Trabajadores_en_lista (){
+
+    public void Imprimir_Trabajadores_en_lista() {
     }
 }
--- trabajadores.java
+++ trabajadores.java
@@ -3,13 +3,13 @@
  * Hereda a docente y no docente, permite darles los atributos a sus clases hijas y crear objetod de este tipo.
  */
 public class trabajadores {
-    private String nombre;
-    private String identificación;
-    private int salarioBase;
-    private int ausencias_impuntualidades;
-    private int tarifa_diaria;
-    
-    //Constructores de la clase padre 
+    String nombre;
+    String identificación;
+    int salarioBase;
+    int ausencias_impuntualidades;
+    int tarifa_diaria;
+
+    // Constructores de la clase padre
     public trabajadores() {
         this.nombre = "";
         this.identificación = "";
@@ -17,6 +17,7 @@ public class trabajadores {
         this.ausencias_impuntualidades = 0;
         this.tarifa_diaria = salarioBase / 24;
     }
+
     public trabajadores(String nombre, String identificación, int salarioBase, int ausencias_impuntualidades) {
         this.nombre = nombre;
         this.identificación = identificación;
@@ -24,54 +25,67 @@ public class trabajadores {
         this.ausencias_impuntualidades = ausencias_impuntualidades;
         this.tarifa_diaria = salarioBase / 24;
     }
-    //Getters y Setters de los atributos del trabajador
+
+    // Getters y Setters de los atributos del trabajador
     public String getNombre() {
         return nombre;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
     public String getIdentificación() {
         return identificación;
     }
+
     public void setIdentificación(String identificación) {
         this.identificación = identificación;
     }
+
     public int getSalarioBase() {
         return salarioBase;
     }
+
     public void setSalarioBase(int salarioBase) {
         this.salarioBase = salarioBase;
     }
+
     public int getAusencias_impuntualidades() {
         return ausencias_impuntualidades;
     }
+
     public void setAusencias_impuntualidades(int ausencias_impuntualidades) {
         this.ausencias_impuntualidades = ausencias_impuntualidades;
     }
+
     public int getTarifa_diaria() {
         return tarifa_diaria;
     }
+
     public void setTarifa_diaria(int tarifa_diaria) {
         this.tarifa_diaria = tarifa_diaria;
     }
-    
-     //Calculos del salario promedio de los trabadores que será heredado por las clases hijas 
+
+    // Calculos del salario promedio de los trabadores que será heredado por las
+    // clases hijas
     /**
      * @return la cantidad de salario que gana un trabajador promedio
      */
-    public double salario(){ 
+    public double salario() {
 
-		return salarioBase;
+        return salarioBase;
 
-	}
+    }
+
     /**
-     * @return el descuento por las ausencias multiplicado con la tarifa diaria del trabajador 
+     * @return el descuento por las ausencias multiplicado con la tarifa diaria del
+     *         trabajador
      */
-    public double descuentoAusencias(){
+    public double descuentoAusencias() {
 
-		return tarifa_diaria*ausencias_impuntualidades;
+        return tarifa_diaria * ausencias_impuntualidades;
 
-	}
+    }
 
 }
